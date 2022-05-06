915f9a05266fa6882d37d1ab9264f260b69d4a37
rin309 <littering@outlook.jp>
2022-05-06 20:05:05 +0900

    Add files via upload

---
+++ Get-WsusLatestUpdateAndOldestUpdatePerProduct.ps1
@@ -0,0 +1,135 @@
+Function Get-WsusLatestUpdateAndOldestUpdatePerProduct($WsusServer, $CsvPath){
+    # 使用しない項目はコメントアウトした
+    Class WsusProductItem{
+        #$Type
+        $ProhibitsSubcategories
+        $ProhibitsUpdates
+        #$UpdateSource
+        #$UpdateServer
+        $Id
+        $Title
+        $Description
+        #$ReleaseNotes
+        #$DefaultPropertiesLanguage
+        $DisplayOrder
+        $ArrivalDate
+
+        $ParentCategoryTitle
+        $LatestUpdate
+        $OldtestUpdate
+        $UpdatesCount
+    }
+    Function Get-WsusProductItemOfWsusCategory($Category, $Item, $RetryCount){
+        $RetryCount++
+        If ($RetryCount -eq 1){
+            Write-Host "$(Get-Date): $($Item.Title)"
+        }
+        ElseIf ($RetryCount -gt $MaximumRetry){
+            Write-Host "$(Get-Date): $($Item.Title) の更新プログラム取得はスキップされました"
+            Write-Host ""
+
+            Return $Item
+        }
+        Else{
+            Write-Host "$(Get-Date): $($Item.Title) の更新プログラム取得を再試行します ($RetryCount 回目)"
+            Write-Host ""
+        }
+
+        Try {
+            $CurrentUpdateScope = New-Object Microsoft.UpdateServices.Administration.UpdateScope
+            $CurrentUpdateScope.Categories.Add($Category) | Out-Null
+            $Item.UpdatesCount = $WsusServer.GetUpdateCount($CurrentUpdateScope)
+
+            If ($Item.UpdatesCount -ne 0){
+                ForEach($Month in $Months){
+                    $OldtestUpdateUpdateScope = New-Object Microsoft.UpdateServices.Administration.UpdateScope
+                    $OldtestUpdateUpdateScope.Categories.Add($CurrentUpdateScope.Categories[0]) | Out-Null
+                    $OldtestUpdateUpdateScope.FromCreationDate = $Month
+                    $OldtestUpdateUpdateScope.ToCreationDate = $Month.AddMonths(1).AddSeconds(-1)
+
+                    If ($WsusServer.GetUpdateCount($OldtestUpdateUpdateScope) -ne 0){
+                        $Item.OldtestUpdate = (($WsusServer.GetUpdates($OldtestUpdateUpdateScope)) | Sort-Object CreationDate)[0]
+                        Break
+                    }
+                }
+                ForEach($Month in ($Months | Sort-Object -Descending)){
+                    $LatestUpdateScope = New-Object Microsoft.UpdateServices.Administration.UpdateScope
+                    $LatestUpdateScope.Categories.Add($CurrentUpdateScope.Categories[0]) | Out-Null
+                    $LatestUpdateScope.FromCreationDate = $Month
+                    $LatestUpdateScope.ToCreationDate = $Month.AddMonths(1).AddSeconds(-1)
+
+                    If ($WsusServer.GetUpdateCount($LatestUpdateScope) -ne 0){
+                        $Item.LatestUpdate = (($WsusServer.GetUpdates($LatestUpdateScope)) | Sort-Object CreationDate -Descending)[0]
+                        Break
+                    }
+                }
+            }
+            Return $Item
+        }
+        Catch [System.Exception]
+        {
+            Write-Host "$(Get-Date): $($Item.Title) の更新プログラムを取得できませんでした"
+            Write-Host $_.Exception
+            Write-Host ""
+
+            Get-WsusProductItemOfWsusCategory $Category $Item $RetryCount
+        }
+    }
+    
+    # 一番古い更新プログラムの日付を指定する
+    $MinimumDate = [datetime]"2003/1/1"
+    $MaximumRetry = 2
+
+    $CurrentDate = $MinimumDate
+    $Months = @()
+    Do{
+        $Months += $CurrentDate
+        $CurrentDate = $CurrentDate.AddMonths(1)
+    }
+    While($CurrentDate -lt (Get-Date -Day 1))
+
+    Write-Host "WSUSサーバー上の更新プログラム ($($WsusServer.GetUpdateCount()) 件) の製品一覧を取得します"
+
+    # 下記のカテゴリー配下の更新プログラムを抽出する
+    # - Windows
+    # - Office
+    # - SQL Server
+    # - Developer Tools, Runtimes, and Redistributables
+    (Get-WsusProduct -TitleIncludes "Microsoft" | Where-Object {$_.Product.Id -eq "56309036-4c77-4dd9-951a-99ee9c246a94"}).Product.GetSubcategories() | Where-Object {$_.Type -eq "ProductFamily" -and ($_.Id -eq "6964aab4-c5b5-43bd-a17d-ffb4346a8e1d" -or $_.Id -eq "477b856e-65c4-4473-b621-a8b230bb70d9" -or $_.Id -eq "48ce8c86-6850-4f68-8e9d-7dc8535ced60" -or $_.Id -eq "0a4c6c73-8887-4d7f-9cbe-d08fa8fa9d1e")} | Sort-Object @{expression={switch ($_.Id){"6964aab4-c5b5-43bd-a17d-ffb4346a8e1d"{1};"477b856e-65c4-4473-b621-a8b230bb70d9"{2};"48ce8c86-6850-4f68-8e9d-7dc8535ced60"{3};"0a4c6c73-8887-4d7f-9cbe-d08fa8fa9d1e"{4}default{5}}}} | ForEach-Object {
+        $ParentCategoryTitle = $_.Title
+        $_.GetSubcategories() | ForEach-Object {
+            $Item = New-Object WsusProductItem
+            #$Item.Type = $_.Type
+            $Item.ProhibitsSubcategories = $_.ProhibitsSubcategories
+            $Item.ProhibitsUpdates = $_.ProhibitsUpdates
+            #$Item.UpdateSource = $_.UpdateSource
+            #$Item.UpdateServer = $_.UpdateServer
+            $Item.Id = $_.Id
+            $Item.Title = $_.Title
+            $Item.Description = $_.Description
+            #$Item.ReleaseNotes = $_.ReleaseNotes
+            #$Item.DefaultPropertiesLanguage = $_.DefaultPropertiesLanguage
+            $Item.DisplayOrder = $_.DisplayOrder
+            $Item.ArrivalDate = $_.ArrivalDate
+
+            $Item.ParentCategoryTitle = $ParentCategoryTitle
+
+            $Item = Get-WsusProductItemOfWsusCategory $_ $Item 0
+            
+            # 再利用する場合は変数に格納する
+            # $Script:WsusProcutList += $Item
+            $Item | Select-Object Title, UpdatesCount, @{Name="LatestUpdate.Title";Expression={$_.LatestUpdate.Title}}, @{Name="LatestUpdate.CreationDate";Expression={$_.LatestUpdate.CreationDate}}, @{Name="LatestUpdate.LegacyName";Expression={$_.LatestUpdate.LegacyName}}, @{Name="OldtestUpdate.Title";Expression={$_.OldtestUpdate.Title}}, @{Name="OldtestUpdate.CreationDate";Expression={$_.OldtestUpdate.CreationDate}}, @{Name="OldtestUpdate.LegacyName";Expression={$_.OldtestUpdate.LegacyName}}, Id, ParentCategoryTitle | Export-Csv -Path $CsvPath -Encoding UTF8 -NoTypeInformation -Append
+        }
+    }
+}
+
+# 再利用する場合は変数を初期化する
+# $Script:WsusProcutList = @()
+
+$WsusServer = Get-WsusServer localhost -port 8530
+$CsvPath = "$env:USERPROFILE\Desktop\Get-WsusLatestUpdateAndOldestUpdatePerProduct.csv"
+
+Get-WsusLatestUpdateAndOldestUpdatePerProduct -WsusServer $WsusServer -CsvPath $CsvPath
+
+# 再利用するサンプル
+#$WsusProcutList | Select-Object Title, UpdatesCount, @{Name="LatestUpdate.Title";Expression={$_.LatestUpdate.Title}}, @{Name="LatestUpdate.CreationDate";Expression={$_.LatestUpdate.CreationDate}}, @{Name="LatestUpdate.LegacyName";Expression={$_.LatestUpdate.LegacyName}}, @{Name="OldtestUpdate.Title";Expression={$_.OldtestUpdate.Title}}, @{Name="OldtestUpdate.CreationDate";Expression={$_.OldtestUpdate.CreationDate}}, @{Name="OldtestUpdate.LegacyName";Expression={$_.OldtestUpdate.LegacyName}}, Id, ParentCategoryTitle | Export-Csv -Path "$env:USERPROFILE\Desktop\Get-WsusLatestUpdateAndOldestUpdatePerProduct.csv" -Encoding UTF8 -NoTypeInformation
---
+++ SQL Server.md
@@ -0,0 +1,16 @@
+| Title 	| UpdatesCount 	| LatestUpdate.Title 	| LatestUpdate.CreationDate 	| LatestUpdate.LegacyName 	| OldtestUpdate.Title 	| OldtestUpdate.CreationDate 	| OldtestUpdate.LegacyName 	| Id 	|
+|---	|---	|---	|---	|---	|---	|---	|---	|---	|
+| Microsoft   SQL Server 2012 	| 14 	| SQL Server 2012 Service Pack 4   (KB4018073) 	| 2018/1/26 19:09 	|  	| SQL Server 2012 RTM のセキュリティ更新プログラム   (KB2716442) 	| 2012/10/9 17:00 	|  	| 56750722-19b4-4449-a547-5b68f19eee38 	|
+| Microsoft SQL Server 2014 	| 10 	| SQL Server 2014 Service Pack 3 (KB4022619) 	| 2018/10/30 18:00 	|  	| SQL Server 2014 のセキュリティ更新プログラム (KB2977316) 	| 2014/8/12 17:00 	|  	| caab596c-64f2-4aa9-bbe3-784c6e2ccf9c 	|
+| Microsoft SQL Server 2016 	| 4 	| SQL Server 2016 Service Pack 3 (KB5003279) 	| 2021/11/22 17:07 	|  	| SQL Server 2016 Analysis Services の緊急更新プログラム (KB3179258) 	| 2016/9/23 22:54 	|  	| 93f0b0bc-9c20-4ca5-b630-06eb4706a447 	|
+| Microsoft SQL Server 2017 	| 0 	|  	|  	|  	|  	|  	|  	| ca6616aa-6310-4c2d-a6bf-cae700b85e86 	|
+| Microsoft SQL Server 2019 	| 0 	|  	|  	|  	|  	|  	|  	| 97b8b817-fce1-44e4-ac26-61127d4604c0 	|
+| Microsoft SQL Server   Management Studio v17 	| 1 	| Microsoft SQL Server Management Studio 17.0 RC3 	| 2017/3/28 17:00 	|  	| Microsoft SQL Server Management Studio 17.0 RC3 	| 2017/3/28 17:00 	|  	| dee854fd-e9d2-43fd-bbc3-f7568e3ce324 	|
+| Microsoft SQL Server   Management Studio v18 	| 3 	| SQL Server Management Studio 18.11.1 	| 2022/3/15 16:58 	|  	| SQL Server Management Studio 18.1 	| 2019/6/20 21:43 	|  	| 05baacdd-c45a-4e37-b95c-ee0453061d89 	|
+| SQL Server 2000 	| 17 	| SQL Server Reporting Services 2000 Service Pack 2   のセキュリティ更新プログラム (KB983814) 	| 2012/10/9 17:00 	|  	| SQL Server 2000 Service Pack 4 のセキュリティ更新プログラム (KB948111) 	| 2008/7/8 17:00 	|  	| 7145181b-9556-4b11-b659-0162fa9df11f 	|
+| SQL Server 2005 	| 52 	| SQL Server 2005 Service Pack 4 のセキュリティ更新プログラム (KB2716429) 	| 2012/10/9 17:00 	|  	| Microsoft SQL Server 2005 Service Pack 1 (KB 913090) 	| 2007/3/7 18:00 	|  	| 60916385-7546-4e9b-836e-79d65e517bab 	|
+| SQL Server 2008 R2 	| 12 	| Microsoft SQL Server 2008 R2 Service Pack 3 (KB2979597) 	| 2016/7/1 18:00 	|  	| SQL Server 2008 R2 のセキュリティ更新プログラム (KB2494088) 	| 2011/6/14 17:00 	|  	| bb7bc3a7-857b-49d4-8879-b639cf5e8c3c 	|
+| SQL Server 2008 	| 18 	| Microsoft SQL Server 2008 Service Pack 4 (KB2979596) 	| 2016/7/1 18:00 	|  	| Microsoft SQL Server 2008 Service Pack 1 (KB968369) 	| 2009/5/26 17:00 	|  	| c5f0b23c-e990-4b71-9808-718d353f533a 	|
+| SQL Server 2012 Product   Updates for Setup 	| 2 	| SQL Server 2012 Service Pack 1 の更新プログラム (スマート セットアップ)   (KB2793634) 	| 2013/2/26 18:00 	|  	| SQL Server 2012 Service Pack 1 Setup Update (KB2674319) 	| 2012/11/27 18:00 	|  	| 7fe4630a-0330-4b01-a5e6-a77c7ad34eb0 	|
+| SQL Server 2014-2016 Product   Updates for Setup 	| 2 	| SQL Server 2016 MSVCRT の前提条件の重要な更新プログラム KB3164398 	| 2016/9/7 22:20 	|  	| SQL Server 2016 MSVCRT の前提条件の重要な更新プログラム KB3164398 	| 2016/6/9 20:00 	|  	| 892c0584-8b03-428f-9a74-224fcd6887c0 	|
+| SQL Server Feature Pack 	| 4 	| MSXML 6.0 RTM セキュリティ更新プログラム (925673) 	| 2012/4/4 22:42 	|  	| Microsoft SQL Server 2005 Books Online (日本語版) (2007 年 5 月) 	| 2007/8/2 17:00 	|  	| c96c35fc-a21f-481b-917c-10c4f64792cb 	|
